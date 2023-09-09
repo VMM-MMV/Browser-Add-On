1824457493e2e3a00ff5d6ebc6ca218a9ab6c2b7
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2023-09-09 12:26:39 +0300

    Update content.js
    Initiall commit

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
 // Get the current page's URL
 var currentURL = window.location.href;
 
-// Check if the URL contains '/shorts/'
 if (currentURL.includes('/shorts/')) {
-    // Replace '/shorts/' with '/watch?v='
     var newURL = currentURL.replace('/shorts/', '/watch?v=');
     
     // Redirect to the new URL
